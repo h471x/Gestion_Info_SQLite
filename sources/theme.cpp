c2d38e95355d074f38de3c1b73359b3b0f6b4af4
Hatix Ntsoa <hatixntsoa@gmail.com>
2023-10-15 02:51:31 +0300

    Added alternating colors for both dark & light theme for the actions buttons

--- sources/theme.cpp
+++ sources/theme.cpp
@@ -22,10 +22,24 @@ void ThemeSwitcher::toggleTheme()
         ui->MenuBtn->setIcon(QIcon(":/darkicon/icons/dark/menu.svg"));
         ui->AdminBtn->setIcon(QIcon(":/darkicon/icons/dark/user-circle.511x512.png"));
         ui->FindBtn->setIcon(QIcon(":/darkicon/icons/dark/search.512x512.png"));
+        ui->SearchBtn->setIcon(QIcon(":/darkicon/icons/dark/search.512x512.png"));
         ui->FlowsBtn->setIcon(QIcon(":/darkicon/icons/dark/location-arrow.512x512.png"));
         ui->OptionsBtn->setIcon(QIcon(":/darkicon/icons/dark/gear.512x512.png"));
         ui->HelpBtn->setIcon(QIcon(":/darkicon/icons/dark/question-circle.512x512.png"));
         ui->NotifBtn->setIcon(QIcon(":/darkicon/icons/dark/bell.448x512.png"));
+
+        for (int row = 0; row < ui->tableWidget->rowCount(); ++row) {
+            // Set up the columns you want to display manually
+            QList<int> selectedColumns = {0, 1, 2, 3, 4}; // Replace with the column indexes you want to display
+            int tableColumnCount = selectedColumns.size();
+            if (row % 2 == 0) {
+                ui->tableWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #c5cad6;");
+                ui->findWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #c5cad6;");
+            } else {
+                ui->tableWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #a2a6ae;");
+                ui->findWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #a2a6ae;");
+            }
+        }
 
         isDark = false;
 
@@ -42,10 +56,24 @@ void ThemeSwitcher::toggleTheme()
         ui->MenuBtn->setIcon(QIcon(":/icons/icons/menu.svg"));
         ui->AdminBtn->setIcon(QIcon(":/icons/icons/user-circle.511x512.png"));
         ui->FindBtn->setIcon(QIcon(":/icons/icons/search.512x512.png"));
+        ui->SearchBtn->setIcon(QIcon(":/icons/icons/search.512x512.png"));
         ui->FlowsBtn->setIcon(QIcon(":/icons/icons/location-arrow.512x512.png"));
         ui->OptionsBtn->setIcon(QIcon(":/icons/icons/gear.512x512.png"));
         ui->HelpBtn->setIcon(QIcon(":/icons/icons/question-circle.512x512.png"));
         ui->NotifBtn->setIcon(QIcon(":/icons/icons/bell.448x512.png"));
+
+        for (int row = 0; row < ui->tableWidget->rowCount(); ++row) {
+            // Set up the columns you want to display manually
+            QList<int> selectedColumns = {0, 1, 2, 3, 4}; // Replace with the column indexes you want to display
+            int tableColumnCount = selectedColumns.size();
+            if (row % 2 == 0) {
+                ui->tableWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #272727;");
+                ui->findWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #272727;");
+            } else {
+                ui->tableWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #1f1f1f;");
+                ui->findWidget->cellWidget(row, tableColumnCount)->setStyleSheet("background-color: #1f1f1f;");
+            }
+        }
 
         isDark = true;
     }
